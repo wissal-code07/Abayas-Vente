6774c5d3b3660ce5dd889d60edd24e5379b1ada8
wissal-code07 <152210538+wissal-code07@users.noreply.github.com>
2026-08-23 19:27:52 +0100

    Fix import paths for LoginForm and RegisterForm

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import LoginForm from "../components/auth/LoginForm";
-import RegisterForm from "../components/auth/RegisterForm";
+import LoginForm from "../components/Auth/LoginForm";
+import RegisterForm from "../components/Auth/RegisterForm";
 import "./AuthPage.css";
 
 export default function AuthPage({ navigate, defaultTab = "login", onLoginSuccess }) {
